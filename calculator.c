371a74beb08ee53abb9744ace2c902aeb53dfddd
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-02-27 23:40:31 +0530

    Simple Calculator
    Gives the user an opportunity to perform basic operations like addition, subtraction, multiplication and division.

---
+++ calculator.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    float n1, n2, res=0.0f;
+    char op;
+    printf("Calculator\n");
+    printf("----------------------------\n");
+    printf("Enter [no.1] [+ - * /] [no.2]\n");
+    scanf("%f %c %f", &n1, &op, &n2);
+    switch(op)
+    {
+        case '+':
+            res = n1 + n2;
+            break;
+
+        case '-':
+            res = n1 - n2;
+            break;
+
+        case '*':
+            res = n1 * n2;
+            break;
+
+        case '/':
+            res = n1 / n2;
+            break;
+
+        default:
+            printf("Invalid operator");
+    }
+    printf("%.2f %c %.2f = %.2f", n1, op, n2, res);
+
+    return 0;
+}
